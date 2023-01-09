7973df4d2aec59b6ee2b9ca56a88b9b50d280af5
Derek Soulliere <derek.soulliere@gmail.com>
2023-01-09 15:35:22 -0500

    Add Braintree Public SDK to services

--- config/services.php
+++ config/services.php
@@ -31,4 +31,13 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'braintree' => [
+        'environment' => env('BRAINTREE_ENVIRONMENT'),
+        'merchant_id' => env('BRAINTREE_MERCHANT_ID'),
+        'public_key' => env('BRAINTREE_PUBLIC_KEY'),
+        'private_key' => env('BRAINTREE_PRIVATE_KEY'),
+        'default_plan_id' => env('BRAINTREE_DEFAULT_PLAN_ID'),
+    ],
+
+
 ];
